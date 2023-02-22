5af73dc93af18230b7443514287879ffb138f694
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-22 18:40:50 +0100

    new commit found react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
+import React from 'react';
+// import './App.css';
+import { useRoutes } from 'react-router-dom';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeCards from '../src/Components/HomeCards/HomeCards.jsx';
+
 
 function App() {
 
-  const router = createBrowserRouter([{
-    path: "/",
-    element: <HomePage />
-  }, {
-    path: "/Stars",
-    element: <Stars />
-  }, {
-    path: "/Constellations",
-    element: <Constellations />
-  }, {
-    path: "/Tattoos",
-    element: <Tattoos />
-  }
-  ])
+  // const router = createBrowserRouter([{
+  //   path: "/",
+  //   element: <HomePage />
+  // }, {
+  //   path: "/Stars",
+  //   element: <Stars />
+  // }, {
+  //   path: "/Constellations",
+  //   element: <Constellations />
+  // }, {
+  //   path: "/Tattoos",
+  //   element: <Tattoos />
+  // }
+  // ])
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <HomeCards/>
     </div>
   );
 }
---
+++ src/Components/HomeCards/HomeCards.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+export default function BasicCard() {
+  return (
+    <div>hola sdbajdbhcxkancx</div>
+  );
+}
