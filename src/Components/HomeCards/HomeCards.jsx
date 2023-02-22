c3772ad222cd682c1afbc98274e9d3fed8d0ba6a
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-22 22:51:42 +0100

    new card

--- src/Components/HomeCards/HomeCards.jsx
+++ src/Components/HomeCards/HomeCards.jsx
@@ -1,53 +1,104 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
-import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
+import Favorite from "@mui/icons-material/Favorite";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Image from "../../Assest/unsplash.jpg";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import "@fontsource/poppins";
 
-export default function BasicCard() {
+export default function ContainerResponsive() {
+    const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
   return (
-    <Card variant="outlined" sx={{ width: 320 }}>
-      <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
-        Yosemite National Park
-      </Typography>
-      <Typography level="body2">April 24 to May 02, 2021</Typography>
-      <IconButton
-        aria-label="bookmark Bahamas Islands"
-        variant="plain"
-        color="neutral"
-        size="sm"
-        sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+    <Box  sx={{ p:16, m:'auto', width:'30%', minHeight: 350 }}>
+      <Card elevation={3}
+        
+        sx={(theme) => ({
+          bgcolor: 'RGB(117, 44, 160, 0.85)',
+          width: 300,
+          gridColumn: "span 2",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          overflow: "hidden",
+          gap: "clamp(0px, (100% - 360px + 32px) * 999, 16px)",
+          transition: "transform 0.3s, border 0.3s",
+          "&:hover": {
+            borderColor: theme.vars.palette.primary.outlinedHoverBorder,
+            transform: "translateY(-2px)",
+          },
+          "& > *": { minWidth: "clamp(0px, (360px - 100%) * 999,100%)" },
+        })}
       >
-        <BookmarkAdd />
-      </IconButton>
-      <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
-        <img
-          src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
-          srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
-          loading="lazy"
-          alt=""
-        />
-      </AspectRatio>
-      <Box sx={{ display: 'flex' }}>
-        <div>
-          <Typography level="body3">Total price:</Typography>
-          <Typography fontSize="lg" fontWeight="lg">
-            $2,900
-          </Typography>
-        </div>
-        <Button
-          variant="solid"
-          size="sm"
-          color="primary"
-          aria-label="Explore Bahamas Islands"
-          sx={{ ml: 'auto', fontWeight: 600 }}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            maxWidth: 200,
+          }}
         >
-          Explore
-        </Button>
-      </Box>
-    </Card>
+          <AspectRatio
+            variant="soft"
+            sx={{
+              "--AspectRatio-paddingBottom":
+                "clamp(0px, (100% - 200px) * 999, 250px)",
+              pointerEvents: "none",
+            }}
+          >
+            <img alt="" src={Image} />
+          </AspectRatio>
+          <Box sx={{ display: "flex", gap: 1.5, mt: "auto" }}>
+            <div>
+              <Typography fontFamily='poppins'  textColor='white' fontWeight="lg" level="body2">
+                Via lactea
+              </Typography>
+              <Typography fontFamily='poppins' textColor='white' level="body2">Spaceman</Typography>
+              
+            </div>
+            <Link
+              href="#dribbble-shot"
+              level="body3"
+              underline="none"
+              startDecorator={<Favorite />}
+              sx={{
+                color: "#fffb5c",
+                fontWeight: "md",
+                ml: "auto",
+                "&:hover": {color:'red'},
+              }}
+            ></Link>
+            <Link onClick={handleToggle}
+              href="#dribbble-shot"
+              level="body3"
+              underline="none"
+              startDecorator={<AddShoppingCartIcon />}
+              sx={{
+                color: "#fffb5c",
+                fontWeight: "md",
+                "&:hover": {color:'#1D4BBB'},
+              }}
+            ></Link>
+            
+          </Box>
+        </Box>
+      </Card>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Box>
   );
 }
