02e9ac7a3c38890b07aff396aaf7368a460a6929
Raul Villar <raultheviking@gmail.com>
2023-02-23 16:25:13 +0100

    Fixed Cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from './Pages/HomePage/HomePage';
 import Stars from './Pages/Stars/Stars';
 import Constellations from './Pages/Constellations/Constellations';
-import Tattoos from './Pages/Tattoos/Tattoos'
 
 function App() {
 
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -28,42 +28,44 @@ export default function HomePage() {
 
 
   return (
-    <div className='card-container'>
-      <div style={{ display: 'flex', justifyContent: "space-between"}}>
-        <h1 style={{ color: "white"}}>Estrellas</h1>
-        <h2 style={{ color: "white"}}>Ver más</h2>
-      </div>
-      
-      <div className="each-card-container" style={{ display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
-        {stars.map(item => (
-          <div key={item.id}>
-            <HomeCards
-              Image={item.image}
-              Title={item.name}
-              price={item.price}
-              size={item.size}
-              description={item.description}
-            />
-          </div>
-        )).splice (4)}
-      </div>
-      <div style={{ display: 'flex', justifyContent: "space-between"}}>
-        <h1 style={{ color: "white"}}>Constelaciones</h1>
-        <h2 style={{ color: "white"}}>Ver más</h2>
-      </div>
-      
-      <div style={{ display: 'flex', flexDirection: 'row', height: "500px", justifyContent: "center"}}>
-        {constellations.map(item => (
-          <div key={item.id}>
-            <HomeCards
-              Image={item.image}
-              Title={item.name}
-              price={item.price}
-              size={item.size}
-              description={item.description}
-            />
-          </div>
-        )).splice (4)}
+    <div>
+      <div className='card-container'>
+        <div style={{ display: 'flex', justifyContent: "space-between"}}>
+          <h1 style={{ color: "white"}}>Estrellas</h1>
+          <h2 style={{ color: "white"}}>Ver más</h2>
+        </div>
+        
+        <div className="each-card-container" style={{ display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
+          {stars.map(item => (
+            <div key={item.id}>
+              <HomeCards
+                Image={item.image}
+                Title={item.name}
+                price={item.price}
+                size={item.size}
+                description={item.description}
+              />
+            </div>
+          )).splice (4)}
+        </div>
+        <div style={{ display: 'flex', justifyContent: "space-between"}}>
+          <h1 style={{ color: "white"}}>Constelaciones</h1>
+          <h2 style={{ color: "white"}}>Ver más</h2>
+        </div>
+        
+        <div style={{ display: 'flex', flexDirection: 'row', height: "500px", justifyContent: "center"}}>
+          {constellations.map(item => (
+            <div key={item.id}>
+              <HomeCards
+                Image={item.image}
+                Title={item.name}
+                price={item.price}
+                size={item.size}
+                description={item.description}
+              />
+            </div>
+          )).splice (4)}
+        </div>
       </div>
     </div>
   )
